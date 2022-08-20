e5d48994dd7ec9282879c0347dc16c46dbf62b78
lnara404 <naas0974@gmail.com>
2022-08-21 03:13:42 +0900

    :tada: Setting sleact project

---
+++ webpack.config.ts
@@ -0,0 +1,104 @@
+import path from 'path';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import webpack, { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config: Configuration = {
+  name: 'sleact',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'hidden-source-map' : 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], //babel 적용 확장자
+    alias: {
+      '@hooks': path.resolve(__dirname, 'hooks'),
+      '@components': path.resolve(__dirname, 'components'),
+      '@layouts': path.resolve(__dirname, 'layouts'),
+      '@pages': path.resolve(__dirname, 'pages'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@typings': path.resolve(__dirname, 'typings'),
+    },
+  },
+  //결과물을 여러개 만들 수 있음
+  entry: {
+    app: './client',
+    //app2: './client',
+    //app3: './client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['last 2 chrome versions'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+          env: {
+            development: {
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+          },
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+      {
+        //css를 js로 변경해줌
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    //typescript 설정
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      // eslint: {
+      //   files: "./src/**/*",
+      // },
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  //결과물
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3090,
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+  },
+};
+
+if (isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        useURLPolyfill: true,
+      },
+    }),
+  );
+}
+if (!isDevelopment && config.plugins) {
+}
+
+export default config;
