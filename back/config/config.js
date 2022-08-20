4d47e6181dd29ba04c2160509ccc6ff5bb89071a
lnara404 <naas0974@gmail.com>
2022-08-21 04:00:31 +0900

    :memo: Setting backend

---
+++ back/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+console.log('MYSQL_PASSWORD', process.env.MYSQL_PASSWORD);
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "sleact",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "sleact",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": process.env.MYSQL_PASSWORD,
+    "database": "sleact",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
