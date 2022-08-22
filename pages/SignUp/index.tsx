0a262f3fc3705eafde803c42c2febf05592d0c02
lnara404 <naas0974@gmail.com>
2022-08-22 22:56:27 +0900

    :lipstick: Add signup function

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -2,15 +2,39 @@ import React, { useCallback, useState } from 'react';
 import { Header,Form, Label, Input, Error, Button, Success, LinkContainer } from './style.styled';
 
 const SignUp = () => {
-  const [email] = useState('');
-  const [nickname] = useState('');
-  const [password] = useState('');
-  const [passwordCheck] = useState('');
-  const onChangeEmail = useCallback(() => {}, []);
-  const onChangeNickname = useCallback(() => {}, []);
-  const onChangePassword = useCallback(() => {}, []);
-  const onChangePasswordCheck = useCallback(() => {}, []);
-  const onSubmit = useCallback(() => {}, []);
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismathError, setMismathError] = useState(false);
+  // useCallback으로 감싸지 않아도 동작을 안하는건 아니지만 SignUp 재실행되면서 매번 함수들이 재생성되는걸 막기위해서 감싸준다. (리렌더링이 많이 일어남, 디버깅하기가 어려움)
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+    setMismathError(e.target.value !== passwordCheck);
+  }, [passwordCheck]);
+
+  const onChangePasswordCheck = useCallback((e) => {
+    setPasswordCheck(e.target.value);
+    setMismathError(e.target.value !== password);
+  }, []);
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault(); //SPA 필수
+    if(!mismathError) {
+      console.log('서버로 회원가입하기');
+    }
+    // useCallback 안에 함수를 [email, nickname, password, passwordCheck] 값들이 하나라도 바뀔 때 까지 캐싱한다.
+    // [email, nickname, password, passwordCheck] 값들이 하나라도 바뀌면 안에 함수를 새로만들고 그렇지않으면 이전 함수를 계속 쓴다.
+    console.log(email, nickname, password, passwordCheck)
+  }, [email, nickname, password, passwordCheck]);
 
   return (
     <div id="container">
@@ -45,9 +69,10 @@ const SignUp = () => {
               onChange={onChangePasswordCheck}
             />
           </div>
-          {/*
-          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+
+          {mismathError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {/*
           {signUpError && <Error>{signUpError}</Error>}
           {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
           */}
